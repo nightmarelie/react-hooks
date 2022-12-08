ff65f7e070c784dc8a56e0350157efedd9087ebb
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-12-07 21:58:06 -0800

    feat: add firestore query

---
+++ src/hooks/useFirestoreQuery.ts
@@ -0,0 +1,45 @@
+function useFirestoreQuery(query) {
+  // Our initial state
+  // Start with an "idle" status if query is falsy, as that means hook consumer is
+  // waiting on required data before creating the query object.
+  // Example: useFirestoreQuery(uid && firestore.collection("profiles").doc(uid))
+  const initialState = {
+    status: query ? "loading" : "idle",
+    data: undefined,
+    error: undefined,
+  };
+  // Setup our state and actions
+  const [state, dispatch] = useReducer(reducer, initialState);
+  // Get cached Firestore query object with useMemoCompare (https://usehooks.com/useMemoCompare)
+  // Needed because firestore.collection("profiles").doc(uid) will always being a new object reference
+  // causing effect to run -> state change -> rerender -> effect runs -> etc ...
+  // This is nicer than requiring hook consumer to always memoize query with useMemo.
+  const queryCached = useMemoCompare(query, (prevQuery) => {
+    // Use built-in Firestore isEqual method to determine if "equal"
+    return prevQuery && query && query.isEqual(prevQuery);
+  });
+  useEffect(() => {
+    // Return early if query is falsy and reset to "idle" status in case
+    // we're coming from "success" or "error" status due to query change.
+    if (!queryCached) {
+      dispatch({ type: "idle" });
+      return;
+    }
+    dispatch({ type: "loading" });
+    // Subscribe to query with onSnapshot
+    // Will unsubscribe on cleanup since this returns an unsubscribe function
+    return queryCached.onSnapshot(
+      (response) => {
+        // Get data for collection or doc
+        const data = response.docs
+          ? getCollectionData(response)
+          : getDocData(response);
+        dispatch({ type: "success", payload: data });
+      },
+      (error) => {
+        dispatch({ type: "error", payload: error });
+      }
+    );
+  }, [queryCached]); // Only run effect if queryCached changes
+  return state;
+}
