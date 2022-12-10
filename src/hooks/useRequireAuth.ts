d24f2f7ed7dfdecb3b2a83f3d4258f68bd2674ac
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-12-09 16:26:18 -0800

    feat: add require auth

---
+++ src/hooks/useRequireAuth.ts
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+// import { useAuth } from "./useAuth.js";
+// import { useRouter } from "./useRouter.js";
+
+const useAuth = () => ({ user: {} });
+const useRouter = () => ({ push: (_: string) => true });
+
+type RequireAuthProps = {
+  redirectUrl: string;
+};
+
+export const useRequireAuth = ({
+  redirectUrl = "/signup",
+}: RequireAuthProps) => {
+  const auth = useAuth();
+  const router = useRouter();
+  // If auth.user is false that means we're not
+  // logged in and should redirect.
+  useEffect(() => {
+    if (auth.user === false) {
+      router.push(redirectUrl);
+    }
+  }, [auth, router]);
+  return auth;
+};
