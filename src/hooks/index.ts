b01cc1de6b75335535cae16f4bcfcfd24857b375
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-12-14 16:50:17 -0800

    feat: add why did you update

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,4 @@ export * from "./useToggle";
 export * from "./useMemoCompare";
 export * from "./useAsync";
 export * from "./useEventListener";
+export * from "./useWhyDidYouUpdate";
---
+++ src/hooks/useWhyDidYouUpdate.ts
@@ -0,0 +1,43 @@
+import { useEffect, useRef } from "react";
+
+type WhyDidYouUpdate = {
+  name: string;
+  props: Record<string, string>;
+};
+
+type ChangesObj = {
+  from: string;
+  to: string;
+};
+
+export const useWhyDidYouUpdate = ({ name, props }: WhyDidYouUpdate) => {
+  // Get a mutable ref object where we can store props ...
+  // ... for comparison next time this hook runs.
+  const previousProps = useRef<Record<string, string>>({});
+
+  useEffect(() => {
+    if (previousProps.current) {
+      // Get all keys from previous and current props
+      const allKeys = Object.keys({ ...previousProps.current, ...props });
+      // Use this object to keep track of changed props
+      const changesObj: Record<string, ChangesObj> = {};
+      // Iterate through keys
+      allKeys.forEach((key) => {
+        // If previous is different from current
+        if (previousProps.current[key] !== props[key]) {
+          // Add to changesObj
+          changesObj[key] = {
+            from: previousProps.current[key],
+            to: props[key],
+          };
+        }
+      });
+      // If changesObj not empty then output to console
+      if (Object.keys(changesObj).length) {
+        console.log("[why-did-you-update]", name, changesObj);
+      }
+    }
+    // Finally update previousProps with current props for next hook call
+    previousProps.current = props;
+  });
+};
