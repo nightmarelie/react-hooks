9ff721a23cfc38a3a1e77dea7335b1a215f8301e
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-12-18 21:29:40 -0800

    feat: import dep

--- src/hooks/useMedia.ts
+++ src/hooks/useMedia.ts
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 function useMedia(queries, values, defaultValue) {
   // Array containing a media query list for each query
   const mediaQueryLists = queries.map((q) => window.matchMedia(q));
